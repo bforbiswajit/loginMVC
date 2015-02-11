9023d3a562aec2f06b016f4fb23507d2e646572e
Biswajit Bardhan <sunshine.cst.07@gmail.com>
2015-02-11 15:07:39 +0530

    Create backend.php

---
+++ backend.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include("loginController.php");
+
+if(isset($_POST['action']))
+{
+    switch ($_POST['action'])
+    {
+        case "login":
+        $controller = new loginController($_POST['userName'], $_POST['password']);
+        echo $controller->Auth();
+        break;
+    }
+}
+
+?>
